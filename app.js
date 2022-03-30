067d0cc060f9a01f588e16aa2cc9dca72f5f2cf8
birfied <birfychen@gmail.com>
2022-03-30 16:56:54 -0400

    Connect to MongoDB

--- app.js
+++ app.js
@@ -6,17 +6,23 @@ const server = app.listen(port, () => {
     console.log("Server listening on port " + port)
 });
 const path = require('path');
+const bodyParser = require("body-parser");
+const mongoose = require("./database");
+
 
 app.set("view engine", "pug");
 app.set("views", "templates");
 
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 //Routes
 
 const loginRouter = require("./routes/loginRoutes");
+const registerRouter = require("./routes/registerRoutes");
 
 app.use("/login", loginRouter);
+app.use("/register", registerRouter);
 
 app.get("/", middleware.requireLogin, (req, res, next) => {
     var payload = {
---
+++ routes/registerRoutes.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const app = express();
+const router = express.Router();
+const bodyParser = require("body-parser");
+const User = require('../schemas/UserSchema');
+
+app.set("view engine", "pug");
+app.set("views", "templates");
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+router.get("/", (req, res, next) => {
+    res.status(200).render("register");
+});
+
+router.post("/", async (req, res, next) => {
+
+    var firstName = req.body.firstName.trim();
+    var lastName = req.body.lastName.trim();
+    var username = req.body.username.trim();
+    var email = req.body.email.trim();
+    var password = req.body.password;
+
+    var payload = req.body;
+
+    if (firstName && lastName && username && email && password) {
+        // continue until this is finished
+        var user = await User.findOne({
+            $or: [
+                { username : username },
+                { email: email }
+            ]
+                
+        })
+        console.log(user);
+        
+        console.log("Hello");
+    } else {
+        payload.errorMessage = "Make sure each field has a valid value.";
+        res.status(200).render("register", payload);
+    }
+    
+    
+})
+
+module.exports = router;
