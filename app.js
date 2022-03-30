717cbb69ccb473d672407fa6ae490ef521c437dc
birfied <birfychen@gmail.com>
2022-03-30 13:52:11 -0400

    Create login page

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
 const express = require("express");
-
 const app = express();
-
 const port = 3000;
-
+const middleware = require("./middleware");
 const server = app.listen(port, () => {
     console.log("Server listening on port " + port)
 });
+const path = require('path');
 
 app.set("view engine", "pug");
 app.set("views", "templates");
 
-app.get("/", (req, res, next) => {
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Routes
+
+const loginRouter = require("./routes/loginRoutes");
+
+app.use("/login", loginRouter);
+
+app.get("/", middleware.requireLogin, (req, res, next) => {
     var payload = {
         pageTitle : "Home"
     }
